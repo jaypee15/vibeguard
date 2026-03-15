2dccec568ad8ca2127f7a0227d4a413a77723f11
jaypee15 <okoyejohnpaul15@gmail.com>
2026-03-15 23:27:46 +0100

    chore: add file loader

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+mod scanner;
+
 use clap::{Parser, Subcommand};
 use anyhow::Result;
+use scanner::get_files_to_scan;
 
 #[derive(Parser,Debug)]
 #[command(author, version, about, long_about = None)]
@@ -26,7 +29,14 @@ fn main() -> Result<()> {
         Commands::Scan { path, json } => {
             println!("Vibeguard is preparing to scan...");
             println!("Target path: {}", path);
-            println!("JSON output: {}", json);
+            
+            let files = get_files_to_scan(path);
+            
+            println!("Found {} files to scan!", files.len());
+            
+            for file in files.into_iter().take(5) {
+                println!("Scanning: {}", file.display());
+            }
         }
     }
     
